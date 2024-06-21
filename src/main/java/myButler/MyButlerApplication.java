5a2e4c8d67deacb660c7f7ceedf13c6e0f075a69
SSUHYUNKIM <yellosuhyun@naver.com>
2024-06-21 14:08:07 +0900

    chore : 프로젝트 셋업

---
+++ src/main/java/myButler/MyButlerApplication.java
@@ -0,0 +1,11 @@
+package myButler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyButlerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyButlerApplication.class, args);
+    }
+}
---
+++ src/test/java/myButler/MyButlerApplicationTest.java
@@ -0,0 +1,11 @@
+package myButler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MyButlerApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
